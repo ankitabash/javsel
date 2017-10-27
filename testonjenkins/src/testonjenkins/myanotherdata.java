fc982f92297e060847a13126ed54c382acf2773a
ankitabash <niitz.man@gmail.com>
2017-10-27 01:35:40 -0400

    hello

---
+++ testonjenkins/src/testonjenkins/myanotherdata.java
@@ -0,0 +1,15 @@
+package testonjenkins;
+
+import org.testng.annotations.Test;
+
+public class myanotherdata {
+	
+	
+	@Test
+	public void sample()
+	{
+		
+		System.out.println("Hello from demo");
+	}
+
+}
---
+++ testonjenkins/src/testonjenkins/mydata.java
@@ -0,0 +1,14 @@
+package testonjenkins;
+
+import org.testng.annotations.Test;
+
+public class mydata {
+	
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("hello from java");
+	}
+
+}
